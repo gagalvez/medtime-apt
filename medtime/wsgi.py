a26a3146199a5b00aa9fc60afffc4d01f36fac04
gagalvez <gagalvezv@gmail.com>
2025-11-02 16:53:44 -0300

    Creación automática de superusuario para Render

--- medtime/wsgi.py
+++ medtime/wsgi.py
@@ -8,6 +8,7 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
 import os
+from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'medtime.settings')
 
@@ -16,6 +17,17 @@ application = get_wsgi_application()
 try:
     from django.core.management import call_command
     call_command('migrate', interactive=False)
-    print("Migraciones aplicadas automáticamente.")
 except Exception as e:
     print("Error al ejecutar migraciones automáticas:", e)
+
+try:
+    from django.contrib.auth import get_user_model
+    User = get_user_model()
+    if not User.objects.filter(username="admin").exists():
+        User.objects.create_superuser(
+            username="admin",
+            email="admin@example.com",
+            password="admin123"
+        )
+except Exception as e:
+    print("Error al crear superusuario automáticamente:", e)
